fa181ea883a9a6ae96039a7477b4e0dcc769498c
Kavindu Santhusha <ksanthusha20@gmail.com>
2025-06-07 00:25:11 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { MovieProvider } from './context/MovieContext';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 ReactDOM.render(
   <BrowserRouter>
     <MovieProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </MovieProvider>
   </BrowserRouter>,
   document.getElementById('root')
