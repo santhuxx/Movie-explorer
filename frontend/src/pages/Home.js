da10eace26aacba216de589854769d33d6a7dc90
Kavindu Santhusha <ksanthusha20@gmail.com>
2025-06-07 18:52:01 +0530

    Frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -589,9 +589,7 @@ const Home = () => {
             ))}
           </Grid>
         </Box>
-      </Container>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7001636087856196"
-     crossorigin="anonymous"></script>
+      </Container>  
     </Box>
   );
 };
