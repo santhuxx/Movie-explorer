f7ba008ea841c22c0040cab0279a6ebd2ad69570
Kavindu Santhusha <ksanthusha20@gmail.com>
2025-06-11 21:36:32 +0530

    frontend changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,13 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { MovieProvider } from './context/MovieContext';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 
 ReactDOM.render(
   <BrowserRouter>
     <MovieProvider>
       <App />
-      <Analytics />
-      <SpeedInsights />
     </MovieProvider>
   </BrowserRouter>,
   document.getElementById('root')
